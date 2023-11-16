9f387a56b7449c5a78931becd2cae30e9890a933
Viking <djviking@yandex.ru>
2023-11-16 10:42:01 +0300

    start pj

--- src/main/java/com/example/shelter/ShelterBotTgApplication.java
+++ src/main/java/com/example/shelter/ShelterBotTgApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShelterBotTgApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Start project");
+		System.out.println("Start projectt");
 		SpringApplication.run(ShelterBotTgApplication.class, args);
 	}
 
