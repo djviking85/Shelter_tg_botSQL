1b6e64c962656722cbcf957422c370c4576c96e3
Viking <djviking@yandex.ru>
2023-11-21 15:08:17 +0300

    делаем сервисы

---
+++ src/main/java/com/example/shelter/service/ShelterService.java
@@ -0,0 +1,45 @@
+package com.example.shelter.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * This class implements working with a database to receive or receive the necessary information
+ */
+@Slf4j
+@Service
+public class ShelterService {
+    private final ShelterRepository shelterRepository;
+
+    public ShelterService(ShelterRepository shelterRepository) {
+        this.shelterRepository = shelterRepository;
+    }
+
+    /**
+     * Method for obtaining information about the nursery, depending on the selected type
+     *
+     * @param type - Shelter type
+     */
+    public String getInfo(ShelterType type) {
+        Optional<Shelter> shelter = shelterRepository.findFirstByType(type);
+        if (shelter.isPresent()) {
+            return shelter.get().getDescription();
+        }
+        log.info("в базе данных нет питомников с таким животным");
+        return "Тут пока что пусто :(";
+    }
+    /**
+     * Method for obtaining information about the instruction, depending on the selected type
+     * @param type - Shelter type
+     */
+    public String getInstruction(ShelterType type) {
+        Optional<Shelter> shelter = shelterRepository.findFirstByType(type);
+        if (shelter.isPresent()) {
+            return shelter.get().getInstruction();
+        }
+        log.info("в базе данных нет питомников с таким животным");
+        return "Тут пока что пусто :(";
+    }
+}
