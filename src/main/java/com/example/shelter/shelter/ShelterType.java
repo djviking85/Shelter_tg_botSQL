3c51dfde612bd4bdec19118410541f5b71d3aee1
Viking <djviking@yandex.ru>
2023-11-21 15:10:58 +0300

    делаем enum шелтер

---
+++ src/main/java/com/example/shelter/shelter/ShelterType.java
@@ -0,0 +1,8 @@
+package com.example.shelter.shelter;
+
+/**
+ * This is an enum class for defining types of animal shelters
+ */
+public enum ShelterType {
+    DOG, CAT
+}
---
+++ src/main/java/com/example/shelter/shelter/ShelterVolunteerType.java
@@ -0,0 +1,5 @@
+package com.example.shelter.shelter;
+
+public enum ShelterVolunteerType {
+    VOLUNTEER
+}
