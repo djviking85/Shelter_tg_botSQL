a273b12bd7bcd63c30162994f1e881db14c6deda
Viking <djviking@yandex.ru>
2023-11-21 15:03:40 +0300

    делаем сущности

---
+++ src/main/java/com/example/shelter/entity/Shelter.java
@@ -0,0 +1,28 @@
+package com.example.shelter.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+
+/**
+ * This class represents basic information about the shelter
+ * and its relationship to the collection of Animal objects that are in this shelter.
+ * It is used to work with shelter data,
+ * including creating, reading, updating and deleting shelters from the database.
+ */
+@Entity
+@Data
+public class Shelter {
+    @Id
+    @Enumerated(EnumType.STRING)
+    private ShelterType type;
+    private String name;
+    private String description;
+    private String instruction;
+
+
+}
